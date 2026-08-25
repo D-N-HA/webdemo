c02b4ad083fbe248adabb1af3e2524dc5fa3f480
Hà <ngocha2004ct@gmail.com>
2026-08-25 22:48:33 +0700

    Tam tat Google auth

--- ghost_stories/settings.py
+++ ghost_stories/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'allauth.socialaccount.providers.google',
+   # 'allauth.socialaccount.providers.google',
 ]
 
 SITE_ID = 1
